cb0a956808ea8c4cb274cf45f4c2c093efdb012b
sadik <153581673+sadiklaliwala@users.noreply.github.com>
2024-01-20 12:43:21 +0530

    Update urls.py

--- apd/urls.py
+++ apd/urls.py
@@ -48,5 +48,5 @@ urlpatterns = [
       path('delete_admin/<int:admin_id>', views.delete_admin ,name="delete_admin"),
       path('delete_billing/<int:bill_id>', views.delete_billing ,name="delete_billing"),
       path('delete_delivery/<int:d_id>', views.delete_delivery ,name="delete_delivery"),
-
+path()
 ]
