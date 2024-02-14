24495bba1236e34b62f8c04f4ac02f36c9b83506
sadik <153581673+sadiklaliwala@users.noreply.github.com>
2024-02-14 09:42:20 +0530

    s

--- adminsite/urls.py
+++ adminsite/urls.py
@@ -3,28 +3,28 @@ from django.urls import path
 from adminsite import views
 
 urlpatterns = [
-      #all data show 
-      path('show/',views.show, name ="show"),
+      # #all data show 
+      # path('show/',views.show, name ="show"),
       
-      # navbar
-      #path('nav/',views.nav, name ='nav'),
+      # # navbar
+      # #path('nav/',views.nav, name ='nav'),
 
-      path('admindasheboard/', views.admindasheboard,name="admindasheboard"),
+      # path('admindasheboard/', views.admindasheboard,name="admindasheboard"),
       
-      # purchaseform
-      path('purchase_add/',views.purchase_add, name ='purchase_add'),
+      # # purchaseform
+      # path('purchase_add/',views.purchase_add, name ='purchase_add'),
       
-      #work
-      # path('workform/',views.workform, name ='workform'),
+      # #work
+      # # path('workform/',views.workform, name ='workform'),
       
-      #customer
-      path('customer_add/',views.customer_add, name ="customer_add"),
+      # #customer
+      # path('customer_add/',views.customer_add, name ="customer_add"),
       
-      # supplier 
-      path('sup_add/',views.sup_add, name ="sup_add"),
+      # # supplier 
+      # path('sup_add/',views.sup_add, name ="sup_add"),
       
-      # admin 
-      path('admin_add/',views.admin_add, name ="admin_add"),
+      # # admin 
+      # path('admin_add/',views.admin_add, name ="admin_add"),
 
       
       
